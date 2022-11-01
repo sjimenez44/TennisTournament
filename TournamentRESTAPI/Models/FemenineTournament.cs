527910f84b0ab189c7dbdd3d15c85aa85e7325c2
Santiago Jimenez <info@bufferorigin.com>
2022-11-01 13:23:04 -0500

    Add REST API

---
+++ TournamentRESTAPI/Models/FemenineTournament.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace TournamentRESTAPI.Models
+{
+    public class Tournament<T>
+    {
+        public string Name { get; set; }
+        public List<T> Players { get; set; }
+    }
+}
