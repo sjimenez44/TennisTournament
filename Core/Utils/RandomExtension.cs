046d62083705158c3a49dd56c039e5e42e2d78ba
Santiago Jimenez <info@bufferorigin.com>
2022-11-01 12:14:54 -0500

    Add core utils

---
+++ Core/Utils/RandomExtension.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utils
+{
+    public static class RandomExtensions
+    {
+        public static double NextDouble(this Random RandGenerator, double MinValue, double MaxValue)
+        {
+            return RandGenerator.NextDouble() * (MaxValue - MinValue) + MinValue;
+        }
+    }
+}
