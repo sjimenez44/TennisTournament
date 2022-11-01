a34bc8aae1bce2a551427a1e4933b1f99a4cf3ee
Santiago Jimenez <info@bufferorigin.com>
2022-11-01 13:07:41 -0500

    Add tournament feminine or masculine

---
+++ Core/Interfaces/IGame.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Interfaces
+{
+    public interface IGame<T>
+    {
+        T Game(List<T> listOfPlayers);
+    }
+}
---
+++ Core/Services/TournamentMasculine.cs
@@ -0,0 +1,40 @@
+﻿using Core.Interfaces;
+using Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Core.Services
+{
+    public class TournamentMasculine : IGame<MasculinePlayer>
+    {
+        public MasculinePlayer Game(List<MasculinePlayer> listOfPlayers)
+        {
+            ValidatorTournamentMasculine validatorTournament = new ValidatorTournamentMasculine();
+            ValidationResult resultValidation = validatorTournament.Validator(listOfPlayers);
+
+            if (resultValidation.IsValid)
+            {
+                ValidatorPlayerMasculine validatorPlayer = new ValidatorPlayerMasculine();
+                bool validatePlayers = listOfPlayers
+                    .Select(f => validatorPlayer.Validator(f).IsValid)
+                    .All(c => c == true);
+                if (validatePlayers)
+                {
+                    CheckStageMasculine checkStage = new CheckStageMasculine();
+                    List<MasculinePlayer> listStage = checkStage.Check(listOfPlayers);
+                    while (listStage.Count > 1)
+                    {
+                        listStage = checkStage.Check(listStage);
+                    }
+                    return listStage[0];
+                }
+
+                throw new Exception("Verificar los datos de entrada de los jugadores.");
+            }
+
+            throw new Exception(string.Join(Environment.NewLine, resultValidation.ErrorMessage));
+        }
+    }
+}
