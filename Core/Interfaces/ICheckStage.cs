14cb41c0536e5c8a0519d7291280e0418537e1e9
Santiago Jimenez <info@bufferorigin.com>
2022-11-01 12:14:04 -0500

    Add stage checker

---
+++ Core/Interfaces/ICheckStage.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Core.Interfaces
+{
+    public interface ICheckStage<T>
+    {
+        List<T> Check(List<T> players);
+    }
+}
---
+++ Core/Services/CheckStageMasculine.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Core.Interfaces;
+using Core.Models;
+
+namespace Core.Services
+{
+    public class CheckStageMasculine : ICheckStage<MasculinePlayer>
+    {
+        public List<MasculinePlayer> Check(List<MasculinePlayer> players)
+        {
+            int numPlayers = players.Count;
+            if (numPlayers > 1)
+            {
+                List<MasculinePlayer> result = new List<MasculinePlayer>();
+                for (int i = 0; i < numPlayers / 2; i++)
+                {
+                    MasculinePlayer player1 = players[0 + 2 * i];
+                    MasculinePlayer player2 = players[1 + 2 * i];
+                    double valuePlayer1 = player1.Processor.Calculate(player1);
+                    double valuePlayer2 = player1.Processor.Calculate(player2);
+
+                    if (valuePlayer1 > valuePlayer2)
+                    {
+                        result.Add(player1);
+                    }
+                    else
+                    {
+                        result.Add(player2);
+                    }
+                }
+                return result;
+            }
+            return players;
+        }
+    }
+}
